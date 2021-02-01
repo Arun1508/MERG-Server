924615314ae7def56490d66430a8eb7d46dbe79f
Arun1508 <49265519+Arun1508@users.noreply.github.com>
2021-02-01 16:36:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports ={
-    MONGODB: 'mongodb+srv://sam:Sam@123321!@cluster0.uhske.mongodb.net/sam?retryWrites=true&w=majority',
+    MONGODB: 'mongodb+srv://sam:<Password><@cluster0.uhske.mongodb.net/sam?retryWrites=true&w=majority',
     SECRET_KEY: 'some secret key',
               
 }
